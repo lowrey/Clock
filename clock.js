bbc07d5a0f9e6c2376a0eb47834ccc42db931ad8
James Ye <lolz836@hotmail.com>
2013-06-06 22:38:03 +1000

    EVERYTHING

--- clock.js
+++ clock.js
@@ -25,11 +25,14 @@ function determineMeridian(date) {
 }
 
 var dow = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+var months = ["January","February","March","April","May","June","July","August","September","October","November","December"]
 
 function updateTime() {
 	var now = new Date();
 	var date = now.getDate().toString();
 	var month = (now.getMonth()+1).toString();
+	var shortYear = now.getYear().toString();
+	shortYear = shortYear.substr(shortYear.length-2, shortYear.length);
 	
 	if (date.length == 1) {
 		date = "0" + date;
@@ -40,7 +43,8 @@ function updateTime() {
 	
 	document.getElementById("time").innerHTML = getPrettyDate(now);
 	document.getElementById("meridian").innerHTML = determineMeridian(now);
-	document.getElementById("day").innerHTML = dow[now.getDay()] + ", " + date + "/" + month + "/" + now.getFullYear();
+	document.getElementById("day").innerHTML = dow[now.getDay()] + ", " + now.getDate() + " " + months[now.getMonth()] + " " + now.getFullYear();
+	document.getElementById("short").innerHTML = date + "/" + month + "/" + shortYear;
 }
 
 function load() {
@@ -48,30 +52,36 @@ function load() {
 	updateTime();
 }
 
-$(function () {
-    var timer;
-    var fadeInBuffer = false;
-    $(document).mousemove(function () {
-        if (!fadeInBuffer) {
-            if (timer) {
-                clearTimeout(timer);
-                timer = 0;
-            }
-            $('.fade-object').fadeIn();
-            $('html').css({
-                cursor: ''
-            });
-        } else {
-            fadeInBuffer = false;
-        }
+var MOUSE_HIDDEN = false;
 
+function hideMouse() {
 
-        timer = setTimeout(function () {
-            $('.fade-object').fadeOut()
-            $('html').css({
-                cursor: 'none'
-            });
-            fadeInBuffer = true;
-        }, 5000)
-    });
-});
+	//MOUSE_HIDDEN = true;
+	if (MOUSE_HIDDEN && document.body != null) {
+		document.body.className = "nocursor";
+	}
+
+}
+
+var lastX = "Hi";
+var lastY = "Hi";
+window.onmousemove = function(ev) {
+	//doBadStuffSoThatICanCatchAnException();
+	if (ev.x == lastX && ev.y == lastY) {
+		lastX = ev.x;
+		lastY = ev.y;
+		return;
+	}
+	if (MOUSE_HIDDEN) {
+		MOUSE_HIDDEN = false;
+		document.body.className = "";
+		setTimeout(function() {
+			MOUSE_HIDDEN = true;
+			hideMouse()
+		}, 5000);
+	}
+	lastX = ev.x;
+	lastY = ev.y;
+}
+MOUSE_HIDDEN = true;
+hideMouse();
